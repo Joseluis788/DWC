1cd525c01ce9a7dfe2a1bf320e8d7487a024b45a
Joseluis788 <43463998+Joseluis788@users.noreply.github.com>
2022-11-16 14:39:46 +0100

    a

---
+++ DWC/T3P1/Matriculas.js
@@ -0,0 +1,42 @@
+let arrayNumeros = [0,0,0,0];
+let arrayNumerosFake;
+
+for (let i = 0; i <= 9999; i++)
+{
+    arrayNumerosFake = arrayNumeros;
+    arrayNumeros.sort();
+    console.log("ordenado"+arrayNumeros);
+    console.log("desordenado"+arrayNumerosFake);
+    arrayNumeros = arrayNumerosFake;
+    incrementar (arrayNumeros);
+}
+
+function incrementar (arrayNumeros)
+{
+    if (arrayNumeros[3] != 9)
+        arrayNumeros[3]++;
+    else
+        {
+            arrayNumeros[3] = 0;
+            if (arrayNumeros[2] != 9)
+                arrayNumeros[2]++;
+            else
+                {
+                    arrayNumeros[2] = 0;
+                    if (arrayNumeros[1] != 9)
+                        arrayNumeros[1]++;
+                    else
+                    {
+                        arrayNumeros[1] = 0;
+                        if (arrayNumeros[0] != 9)
+                            arrayNumeros[0]++;
+                        else
+                        {
+                            arrayNumeros[1] = 9;
+                            arrayNumeros[2] = 9;
+                            arrayNumeros[3] = 9;
+                        }
+                    }
+                }
+        }
+}
