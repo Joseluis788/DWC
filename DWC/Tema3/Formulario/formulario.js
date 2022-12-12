d9a9d0260ba7e267796188a3329c4a245a11cc5d
Joseluis788 <43463998+Joseluis788@users.noreply.github.com>
2022-12-12 21:08:37 +0100

    a

--- DWC/Tema3/Formulario/formulario.js
+++ DWC/Tema3/Formulario/formulario.js
@@ -1,27 +1,33 @@
-let contador = 0;
-
-
+// Creo botón enviar donde guardo el submit
 let botonEnviar = document.getElementById("botonEnviar");
-
-
-function crearCookie ()
-{
-    if (document.cookie && document.cookie.match('contador'))
-        var contador = parseInt(document.cookie.match(/contador=(\d+)/)[1]);
-    else
-        var contador = 0;
-
-    document.cookie = "contador="+ contador + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+// La función cookie para crear la cookie y actualizarla
+function cookie() {
+    // Si existe la cookie y se llama intentos, asigno a contador el número de intentos
+    if (document.cookie && document.cookie.match('intentos')) {
+        
+        var contador = parseInt(document.cookie.match(/intentos=(\d+)/)[1])
+      } else {
+        // Si no inicializo contador a 0
+        var contador = 0
+      }
+      // Incremento contador
+      contador++
+      // Creo la cookie y la muestro
+      document.cookie = "intentos=" + contador+"; expires=Fri, 31 Dec 9999 23:59:59 GMT"
+      document.getElementById("cookies").innerHTML = document.cookie.match(/intentos=(\d+)/)[1]
+    
 }
 
-
+// El evento listener que envia el formulario y llama la función cookie
 botonEnviar.addEventListener("click", function (){
-    crearCookie();
-    contador++;
-    document.cookie = "contador="+ contador + "; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-    document.getElementById("cookies").innerHTML = "Número de intentos: " + document.cookie.replace(/(?:(?:^|.*;\s*)contador\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    if (confirm("¿Desea enviar el formulario?"))
+    {
+        document.formulario.submit();
+    }
+    cookie();
 })
 
+// La función mayus que pone el nombre y el apellido en mayúsculas y comprueba que no tenga números
 function mayus (id)
 {
     let texto = document.getElementById(id);
@@ -40,12 +46,12 @@ function mayus (id)
     }
         
 }
-
+// Función para mostrar los errores
 function errores (texto)
 {
     document.getElementById("errores").innerHTML = "Errores: "+ texto;
 }
-
+// Función para comprobar que la edad está entre 0 y 105
 function comprobarEdad ()
 {
     let edad = document.getElementById("edad");
@@ -65,7 +71,7 @@ function comprobarEdad ()
             errores("");
         }
 }
-
+// Función para comprobar que el NIF tiene 8 números y una letra con un guión en medio
 function comprobarNIF ()
 {
     let patronNIF = /^[0-9]{8}-[A-Z]+$/
@@ -81,7 +87,7 @@ function comprobarNIF ()
             nif.focus();
         }
 }
-
+// Función para validar el email
 function comprobarEmail ()
 {
     let patronEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
@@ -97,7 +103,7 @@ function comprobarEmail ()
         email.focus();
     }
 }
-
+// Función para comprobar la provincia
 function comprobarProvincia()
 {
     let provincia = document.getElementById("provincia");
@@ -112,7 +118,7 @@ function comprobarProvincia()
             errores("");
         }
 }
-
+// Función para comprobar el teléfono
 function comprobarTelefono()
 {
     let telefono = document.getElementById("telefono");
