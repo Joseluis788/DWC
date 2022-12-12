de28d473f9df9b1bdfbaa650b89272e730dfd428
Joseluis788 <43463998+Joseluis788@users.noreply.github.com>
2022-12-12 11:09:52 +0100

    a

--- DWC/Tema3/Formulario/formulario.js
+++ DWC/Tema3/Formulario/formulario.js
@@ -6,3 +6,9 @@ let botonEnviar = document.getElementById("botonEnviar");
 botonEnviar.addEventListener("click", function (){
     contador++;
 })
+
+function mayus (id)
+{
+    let texto = document.getElementById(id);
+    texto.value = texto.value.toUpperCase();
+}
