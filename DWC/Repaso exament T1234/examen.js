00e4a7090151807b3c2fea5271961c115d967194
Joseluis788 <43463998+Joseluis788@users.noreply.github.com>
2022-12-15 11:02:19 +0100

    a

---
+++ DWC/Repaso exament T1234/examen.js
@@ -0,0 +1,45 @@
+let ventana11;
+let ventana2;
+let ventana3;
+let num = 8;
+
+function abrirVentana1 ()
+{
+    ventana11 = window.open("ventana1.html", "Ventana 1", "width = 300 height = 300");
+    ventana11.document.bgColor = "lightgreen";
+    
+}
+
+function abrirVentana2()
+{
+    ventana2 = window.open("", "Ventana 2", "width = 300 height = 300");
+    ventana2.document.bgColor = "blue";
+    ventana2.moveTo(1900,0);
+}
+
+function abrirVentana3()
+{
+    ventana3 = window.open("", "Ventana 3", "width = 300 height = 300");
+    ventana3.document.bgColor = "pink";
+    ventana3.moveTo(800,2000);
+}
+
+function cerrarVentana1()
+{
+    ventana11.close();
+}
+
+function cerrarVentana2()
+{
+    ventana2.close();
+}
+
+function cerrarVentana3()
+{
+    ventana3.close();
+}
+
+function numeros ()
+{
+    ventana11.document.getElementById("prueba1").innerHTML = num;
+}
